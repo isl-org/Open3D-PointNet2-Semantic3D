0b8d87ca602dda4b852984f2f5b8920b39948b90
Yixing Lao <yixing.lao@gmail.com>
2018-11-08 11:48:25 -0800

    tf grouping compiles

--- tf_ops/grouping/tf_grouping_compile.sh
+++ tf_ops/grouping/tf_grouping_compile.sh
@@ -1,8 +1,17 @@
 #/bin/bash
-/usr/local/cuda-8.0/bin/nvcc tf_grouping_g.cu -o tf_grouping_g.cu.o -c -O2 -DGOOGLE_CUDA=1 -x cu -Xcompiler -fPIC
+nvcc tf_grouping_g.cu -o tf_grouping_g.cu.o -c -O2 -DGOOGLE_CUDA=1 -x cu -Xcompiler -fPIC
 
 # TF1.2
-g++ -std=c++11 tf_grouping.cpp tf_grouping_g.cu.o -o tf_grouping_so.so -shared -fPIC -I /home/mathieu/.conda/envs/tensorflow/lib/python2.7/site-packages/tensorflow/include/ -I /usr/local/cuda-8.0/include -lcudart -L /usr/local/cuda-8.0/lib64/ -O2 -D_GLIBCXX_USE_CXX11_ABI=0
+# g++ -std=c++11 tf_grouping.cpp tf_grouping_g.cu.o -o tf_grouping_so.so -shared -fPIC \
+#     -I /home/mathieu/.conda/envs/tensorflow/lib/python2.7/site-packages/tensorflow/include/ \
+#     -I /usr/local/cuda-8.0/include \
+#     -lcudart -L /usr/local/cuda-8.0/lib64/ -O2 -D_GLIBCXX_USE_CXX11_ABI=0
 
 # TF1.4
-#g++ -std=c++11 tf_grouping.cpp tf_grouping_g.cu.o -o tf_grouping_so.so -shared -fPIC -I /usr/local/lib/python2.7/dist-packages/tensorflow/include -I /usr/local/cuda-8.0/include -I /usr/local/lib/python2.7/dist-packages/tensorflow/include/external/nsync/public -lcudart -L /usr/local/cuda-8.0/lib64/ -L/usr/local/lib/python2.7/dist-packages/tensorflow -ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=0
+g++ -std=c++11 tf_grouping.cpp tf_grouping_g.cu.o -o tf_grouping_so.so -shared -fPIC \
+    -I /home/ylao/repo/venvs/semantic3/lib/python3.6/site-packages/tensorflow/include \
+    -I /usr/local/cuda/include \
+    -lcudart -ltensorflow_framework \
+    -L /usr/local/cuda/lib64/ \
+    -L /home/ylao/repo/venvs/semantic3/lib/python3.6/site-packages/tensorflow \
+    -O2 -D_GLIBCXX_USE_CXX11_ABI=0

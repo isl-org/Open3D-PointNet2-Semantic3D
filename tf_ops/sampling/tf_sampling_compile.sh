24c241b7523651eeac1ebd749b22faaf5b23a327
Yixing Lao <yixing.lao@gmail.com>
2018-11-08 11:24:02 -0800

    op compiles

--- tf_ops/sampling/tf_sampling_compile.sh
+++ tf_ops/sampling/tf_sampling_compile.sh
@@ -10,11 +10,11 @@ nvcc tf_sampling_g.cu -o tf_sampling_g.cu.o -c -O2 -DGOOGLE_CUDA=1 -x cu -Xcompi
 #     -O2 -D_GLIBCXX_USE_CXX11_ABI=0
 
 # TF1.4
+# -I /usr/local/lib/python2.7/dist-packages/tensorflow/include/external/nsync/public \
 g++ -std=c++11 tf_sampling.cpp tf_sampling_g.cu.o -o tf_sampling_so.so -shared -fPIC \
-    -I /usr/local/lib/python2.7/dist-packages/tensorflow/include \
-    -I /usr/local/cuda-8.0/include \
-    -I /usr/local/lib/python2.7/dist-packages/tensorflow/include/external/nsync/public \
+    -I /home/ylao/repo/venvs/semantic3/lib/python3.6/site-packages/tensorflow/include \
+    -I /usr/local/cuda/include \
     -lcudart -ltensorflow_framework \
-    -L /usr/local/cuda-8.0/lib64/ \
-    -L/usr/local/lib/python2.7/dist-packages/tensorflow \
+    -L /usr/local/cuda/lib64/ \
+    -L /home/ylao/repo/venvs/semantic3/lib/python3.6/site-packages/tensorflow \
     -O2 -D_GLIBCXX_USE_CXX11_ABI=0

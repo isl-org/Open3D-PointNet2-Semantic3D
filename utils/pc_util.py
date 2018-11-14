d6190f7d5dee30856d08da2110c539ccdc14d248
Yixing Lao <yixing.lao@gmail.com>
2018-11-13 16:18:10 -0800

    clean plotting scripts

--- utils/pc_util.py
+++ utils/pc_util.py
@@ -4,27 +4,6 @@ Author: Charles R. Qi, Hao Su
 Date: November 2016
 """
 
-import numpy as np
-from .plyfile import PlyData, PlyElement
-
-
-def read_ply(filename):
-    """ read XYZ point cloud from filename PLY file """
-    plydata = PlyData.read(filename)
-    pc = plydata["vertex"].data
-    pc_array = np.array([[x, y, z] for x, y, z in pc])
-    return pc_array
-
-
-def write_ply(points, filename, text=True):
-    """ input: Nx3, write points to filename as PLY format. """
-    points = [
-        (points[i, 0], points[i, 1], points[i, 2]) for i in range(points.shape[0])
-    ]
-    vertex = np.array(points, dtype=[("x", "f4"), ("y", "f4"), ("z", "f4")])
-    el = PlyElement.describe(vertex, "vertex", comments=["vertices"])
-    PlyData([el], text=text).write(filename)
-
 
 def write_ply_color(points, labels, out_filename, num_classes=None):
     """ Color (N,3) points with labels (N) within range 0 ~ num_classes-1 as OBJ file """

7e4ca00c1c3e8391a8a5a45f1320db132ebb7839
Yixing Lao <yixing.lao@gmail.com>
2018-11-13 17:39:51 -0800

    num_classes

--- utils/metric.py
+++ utils/metric.py
@@ -6,17 +6,15 @@ class ConfusionMatrix:
     """Streaming interface to allow for any source of predictions. Initialize it, count
        predictions one by one, then print confusion matrix and intersection-union score"""
 
-    def __init__(self, number_of_labels):
-        self.number_of_labels = number_of_labels
-        self.confusion_matrix = np.zeros(
-            shape=(self.number_of_labels, self.number_of_labels)
-        )
+    def __init__(self, num_classes):
+        self.num_classes = num_classes
+        self.confusion_matrix = np.zeros(self.num_classes, self.num_classes)
 
     def count_predicted(self, ground_truth, predicted):
         self.confusion_matrix[ground_truth][predicted] += 1
 
     def get_count(self, ground_truth, predicted):
-        """labels are integers from 0 to number_of_labels-1"""
+        """labels are integers from 0 to num_classes-1"""
         return self.confusion_matrix[ground_truth][predicted]
 
     def get_confusion_matrix(self):
@@ -29,24 +27,22 @@ class ConfusionMatrix:
     def get_intersection_union_per_class(self):
         """returns list of 64-bit floats"""
 
-        matrix_diagonal = [
-            self.confusion_matrix[i][i] for i in range(self.number_of_labels)
-        ]
-        errors_summed_by_row = [0] * self.number_of_labels
-        for row in range(self.number_of_labels):
-            for column in range(self.number_of_labels):
+        matrix_diagonal = [self.confusion_matrix[i][i] for i in range(self.num_classes)]
+        errors_summed_by_row = [0] * self.num_classes
+        for row in range(self.num_classes):
+            for column in range(self.num_classes):
                 if row != column:
                     errors_summed_by_row[row] += self.confusion_matrix[row][column]
-        errors_summed_by_column = [0] * self.number_of_labels
-        for column in range(self.number_of_labels):
-            for row in range(self.number_of_labels):
+        errors_summed_by_column = [0] * self.num_classes
+        for column in range(self.num_classes):
+            for row in range(self.num_classes):
                 if row != column:
                     errors_summed_by_column[column] += self.confusion_matrix[row][
                         column
                     ]
 
-        divisor = [0] * self.number_of_labels
-        for i in range(self.number_of_labels):
+        divisor = [0] * self.num_classes
+        for i in range(self.num_classes):
             divisor[i] = (
                 matrix_diagonal[i]
                 + errors_summed_by_row[i]
@@ -55,16 +51,14 @@ class ConfusionMatrix:
             if matrix_diagonal[i] == 0:
                 divisor[i] = 1
 
-        return [
-            float(matrix_diagonal[i]) / divisor[i] for i in range(self.number_of_labels)
-        ]
+        return [float(matrix_diagonal[i]) / divisor[i] for i in range(self.num_classes)]
 
     def get_overall_accuracy(self):
         """returns 64-bit float"""
         matrix_diagonal = 0
         all_values = 0
-        for row in range(self.number_of_labels):
-            for column in range(self.number_of_labels):
+        for row in range(self.num_classes):
+            for column in range(self.num_classes):
                 all_values += self.confusion_matrix[row][column]
                 if row == column:
                     matrix_diagonal += self.confusion_matrix[row][column]
@@ -79,7 +73,7 @@ class ConfusionMatrix:
     def build_conf_matrix_from_file(self, ground_truth_file, classified_file):
         # read line by line without storing everything in ram
         with open(ground_truth_file, "r") as f_gt, open(classified_file, "r") as f_cl:
-            for index, (line_gt, line_cl) in enumerate(izip(f_gt, f_cl)):
+            for index, (line_gt, line_cl) in enumerate(zip(f_gt, f_cl)):
                 label_gt = int(line_gt)
                 label_cl_ = int(line_cl)
                 label_cl = max([min([label_cl_, 10000]), 1])
@@ -88,14 +82,14 @@ class ConfusionMatrix:
                 if label_cl_ != label_cl:
                     return -1
                 max_label = max([label_gt, label_cl])
-                if max_label > self.number_of_labels:
+                if max_label > self.num_classes:
                     # resize to larger confusion matrix
                     b = np.zeros((max_label, max_label))
-                    for row in range(self.number_of_labels):
-                        for column in range(self.number_of_labels):
+                    for row in range(self.num_classes):
+                        for column in range(self.num_classes):
                             b[row][column] = self.confusion_matrix[row][column]
                     self.confusion_matrix = b
-                    self.number_of_labels = max_label
+                    self.num_classes = max_label
 
                 if label_gt == 0:
                     continue

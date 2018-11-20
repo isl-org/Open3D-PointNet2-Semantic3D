7f1916514fc4356e69b82e0a0226a59123ccfc96
Yixing Lao <yixing.lao@gmail.com>
2018-11-20 12:08:51 -0800

    format

--- preprocess/convert_folder_to_npz.sh
+++ preprocess/convert_folder_to_npz.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-for file in ${1%/}/*.txt
-do
-  filepath=${file:0:${#file}-4}
-  filename=${file:${#1}:${#file}}
-  filename=${filename:0:${#filename}-4}
-  echo "converting file " $filename
-  python convert_to_npz.py --file=$file --out=$2
+for file in ${1%/}/*.txt; do
+    filepath=${file:0:${#file}-4}
+    filename=${file:${#1}:${#file}}
+    filename=${filename:0:${#filename}-4}
+    echo "converting file " $filename
+    python convert_to_npz.py --file=$file --out=$2
 done
-

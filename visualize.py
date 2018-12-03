01fca95499e235fecfa165bd0d21785e0b711817
Yixing Lao <yixing.lao@gmail.com>
2018-12-03 08:21:46 -0800

    skip gt label if test set for visualize

--- visualize.py
+++ visualize.py
@@ -9,19 +9,21 @@ if __name__ == "__main__":
     gt_dir = "dataset/semantic_raw"
     pd_dir = "result/dense"  # Dense prediction
 
-    file_prefix = "sg27_station4_intensity_rgb"
+    file_prefix = "stgallencathedral_station3_intensity_rgb"
     gt_labels_path = os.path.join(gt_dir, file_prefix + ".labels")
     pd_labels_path = os.path.join(pd_dir, file_prefix + ".labels")
     pcd_path = os.path.join(gt_dir, file_prefix + ".pcd")
 
     # Load point cloud and labels
-
     s = time.time()
     pcd = open3d.read_point_cloud(pcd_path)
     print("time read_point_cloud", time.time() - s, flush=True)
 
     s = time.time()
-    gt_labels = load_labels(gt_labels_path)
+    try:
+        gt_labels = load_labels(gt_labels_path)
+    except:
+        gt_labels = None
     print("time load_labels gt", time.time() - s, flush=True)
 
     s = time.time()
@@ -29,11 +31,12 @@ if __name__ == "__main__":
     print("time load_labels pd", time.time() - s, flush=True)
 
     # Eval
-    s = time.time()
-    cm = ConfusionMatrix(9)
-    cm.increment_from_list(gt_labels, pd_labels)
-    cm.print_metrics()
-    print("time eval", time.time() - s, flush=True)
+    if gt_labels is not None:
+        s = time.time()
+        cm = ConfusionMatrix(9)
+        cm.increment_from_list(gt_labels, pd_labels)
+        cm.print_metrics()
+        print("time eval", time.time() - s, flush=True)
 
     # Colorize with predicted label
     s = time.time()
@@ -42,7 +45,8 @@ if __name__ == "__main__":
     open3d.draw_geometries([pcd])
 
     # Colorize with ground-truth label
-    s = time.time()
-    colorize_point_cloud(pcd, gt_labels)
-    print("time colorize_point_cloud", time.time() - s, flush=True)
-    open3d.draw_geometries([pcd])
+    if gt_labels is not None:
+        s = time.time()
+        colorize_point_cloud(pcd, gt_labels)
+        print("time colorize_point_cloud", time.time() - s, flush=True)
+        open3d.draw_geometries([pcd])

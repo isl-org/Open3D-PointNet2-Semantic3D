4faafe1343f4f0cd560a846b0ac9f1068e21dea9
Yixing Lao <yixing.lao@gmail.com>
2018-12-03 11:49:50 -0800

    chunked does not work

--- bench.py
+++ bench.py
@@ -1,9 +1,9 @@
 import numpy as np
-import open3d
 import multiprocessing
 import time
+import itertools
 
-_map_label_to_color = (
+_map_label_to_color = [
     [255, 255, 255],  # white
     [0, 0, 255],  # blue
     [128, 0, 0],  # maroon
@@ -13,26 +13,36 @@ _map_label_to_color = (
     [128, 0, 128],  # purple
     [0, 0, 128],  # navy
     [128, 128, 0],  # olive
-)
+]
 
 
-def _label_to_color(label):
-    global _map_label_to_color
-    return _map_label_to_color[label]
+def _label_chunk_to_color(label_chunk):
+    return [_map_label_to_color[l] for l in label_chunk]
 
 
 def _label_to_colors(labels):
     labels = np.array(labels).astype(np.int32)
     labels = list(labels)
+    num_cpu = multiprocessing.cpu_count()
+    chunk_size = int(np.ceil(len(labels) / float(num_cpu)))
+    label_chunks = [
+        labels[i : i + chunk_size] for i in range(0, len(labels), chunk_size)
+    ]
     with multiprocessing.Pool() as pool:
-        colors = pool.map(_label_to_color, labels)
+        colors_chunks = pool.map(_label_chunk_to_color, label_chunks)
+        colors = list(itertools.chain.from_iterable(colors_chunks))
     return np.array(colors).astype(np.int32)
+
 
 labels = list(np.random.randint(0, 9, size=(5000000,), dtype=np.int32))
 
 s = time.time()
-with multiprocessing.Pool() as pool:
-    colors = pool.map(_label_to_color, labels)
-    colors = np.array(colors)
-    print(colors.shape)
+colors = [_map_label_to_color[l] for l in labels]
+colors = np.array(colors).astype(np.int32)
+print("time to direct", time.time() - s, flush=True)
+
+s = time.time()
+colors_parallel = _label_to_colors(labels)
 print("time to map", time.time() - s, flush=True)
+
+np.testing.assert_array_equal(colors, colors_parallel)

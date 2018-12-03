ec3863344c70107d352121358d9d8dbd12bc5355
Yixing Lao <yixing.lao@gmail.com>
2018-12-03 15:33:43 -0800

    make visulize a tool

--- visualize.py
+++ visualize.py
@@ -1,15 +1,14 @@
 import open3d
-import os
 import time
+import sys
 
 
 if __name__ == "__main__":
-    pcd_dir = "result/dense_colorized"
-    file_prefix = "sg27_station3_intensity_rgb"
-    pcd_path = os.path.join(pcd_dir, file_prefix + ".pcd")
+    if len(sys.argv) <= 1:
+        raise ValueError("Usage: python visulize.py point_cloud_path.pcd")
 
+    pcd_path = sys.argv[1]
     s = time.time()
     pcd = open3d.read_point_cloud(pcd_path)
     print("time read_point_cloud", time.time() - s, flush=True)
-
     open3d.draw_geometries([pcd])

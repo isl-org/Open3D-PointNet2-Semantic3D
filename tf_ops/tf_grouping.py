18ce3268c1d6a2c4bde2ced075ac78e6079dc289
Yixing Lao <yixing.lao@gmail.com>
2018-12-03 19:02:29 -0800

    tf grouping op test working

--- tf_ops/tf_grouping.py
+++ tf_ops/tf_grouping.py
@@ -5,7 +5,9 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(BASE_DIR)
-grouping_module = tf.load_op_library(os.path.join(BASE_DIR, "tf_grouping_so.so"))
+grouping_module = tf.load_op_library(
+    os.path.join(BASE_DIR, "build", "tf_grouping_so.so")
+)
 
 
 def query_ball_point(radius, nsample, xyz1, xyz2):

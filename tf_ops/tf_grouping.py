533e9e744831b495ca2c78e692859d7a6e27a0be
Yixing Lao <yixing.lao@gmail.com>
2018-12-03 19:04:01 -0800

    rename to standard name

--- tf_ops/tf_grouping.py
+++ tf_ops/tf_grouping.py
@@ -6,7 +6,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(BASE_DIR)
 grouping_module = tf.load_op_library(
-    os.path.join(BASE_DIR, "build", "tf_grouping_so.so")
+    os.path.join(BASE_DIR, "build", "libtf_grouping.so")
 )
 
 

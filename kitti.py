9d7b2e8c1d9294604363c480313f746127c6c9d0
Yixing Lao <yixing.lao@gmail.com>
2018-12-10 14:38:04 -0800

    reset_view_point

--- kitti.py
+++ kitti.py
@@ -1,15 +1,13 @@
 import pykitti
 import open3d
 import time
+import numpy as np
 
 basedir = "/home/ylao/data/kitti"
 date = "2011_09_26"
 drive = "0001"
 
-data = pykitti.raw(basedir, date, drive)
-
 pcd = open3d.PointCloud()
-pcd.points = open3d.Vector3dVector(data.get_velo(0)[:, :3])
 
 vis = open3d.Visualizer()
 vis.create_window()
@@ -18,12 +16,18 @@ vis.add_geometry(pcd)
 render_option = vis.get_render_option()
 render_option.point_size = 0.01
 
+data = pykitti.raw(basedir, date, drive)
+to_reset_view_point = True
 for points_with_intensity in data.velo:
     points = points_with_intensity[:, :3]
     pcd.points = open3d.Vector3dVector(points)
+
     vis.update_geometry()
+    if to_reset_view_point:
+        vis.reset_view_point(True)
+        to_reset_view_point = False
     vis.poll_events()
     vis.update_renderer()
-    time.sleep(0.01)
+    time.sleep(0.2)
 
 vis.destroy_window()

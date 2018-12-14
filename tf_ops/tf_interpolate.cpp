bbc8466fdc3965257a09e6f3d9c7f3405fb7fe04
Yixing Lao <yixing.lao@gmail.com>
2018-12-13 18:55:55 -0800

    move out temp var

--- tf_ops/tf_interpolate.cpp
+++ tf_ops/tf_interpolate.cpp
@@ -71,24 +71,21 @@ std::vector<Eigen::Vector3d> buffer_to_eigen_vector(const float *buffer,
 // - target_points (b, n, 3): e.g. (64, 8192, 3), the "3" here is x, y, z
 // - reference_points (b, m, 3): e.g. (64, 1024, 3), the "3" here is x, y, z
 // - dists (b, n, 3): (64, 8192, 3), for each input point in target_points, find
-//                   3 nearest neighbors in base_points and return the
-//                   distances squared, the "3" means "3" nearest neighbors
+//   3 nearest neighbors in base_points and return the distances squared, the
+//   "3" means "3" nearest neighbors
 // - indices (b, n, 3): (64, 8192, 3), for each input point in target_points,
-// find 3
-//                   nearest neighbors in base_points and return the indexes in
-//                   base_points, the "3" means "3" nearest neighbors
+//   find 3 nearest neighbors in base_points and return the indexes in
+//   base_points, the "3" means "3" nearest neighbors
 void threenn_cpu(int batch_size, int n, int m, const float *xyz1,
                  const float *xyz2, float *dists, int *indices) {
     std::vector<int> three_indices;
     std::vector<double> three_dists;
+    open3d::PointCloud target_pcd;
+    open3d::PointCloud reference_pcd;
 
     for (int batch_index = 0; batch_index < batch_size; ++batch_index) {
-        open3d::PointCloud target_pcd;
         target_pcd.points_ = buffer_to_eigen_vector(xyz1, n * 3);
-
-        open3d::PointCloud reference_pcd;
         reference_pcd.points_ = buffer_to_eigen_vector(xyz2, m * 3);
-
         open3d::KDTreeFlann reference_kd_tree(reference_pcd);
 
         for (size_t j = 0; j < n; ++j) {

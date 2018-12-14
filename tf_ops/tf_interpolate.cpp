762025f23728582408b0580515306eca9bac93b4
Yixing Lao <yixing.lao@gmail.com>
2018-12-13 17:56:40 -0800

    remove comments

--- tf_ops/tf_interpolate.cpp
+++ tf_ops/tf_interpolate.cpp
@@ -91,7 +91,6 @@ void threenn_cpu(int b, int n, int m, const float *xyz1, const float *xyz2,
         reference_pcd.points_ = buffer_to_eigen_vector(xyz2, m * 3);
 
         open3d::KDTreeFlann reference_kd_tree(reference_pcd);
-        // for (const Eigen::Vector3d &target_point : target_pcd.points_) {
         for (size_t j = 0; j < n; ++j) {
             std::vector<int> indices;
             std::vector<double> distance2;
@@ -105,49 +104,6 @@ void threenn_cpu(int b, int n, int m, const float *xyz1, const float *xyz2,
             dist[j * 3 + 2] = distance2[2];
         }
 
-        // for (int j = 0; j < n; ++j) {
-        //     float x1 = xyz1[j * 3 + 0];
-        //     float y1 = xyz1[j * 3 + 1];
-        //     float z1 = xyz1[j * 3 + 2];
-        //     double best1 = 1e40;
-        //     double best2 = 1e40;
-        //     double best3 = 1e40;
-        //     int besti1 = 0;
-        //     int besti2 = 0;
-        //     int besti3 = 0;
-        //     for (int k = 0; k < m; ++k) {
-        //         float x2 = xyz2[k * 3 + 0];
-        //         float y2 = xyz2[k * 3 + 1];
-        //         float z2 = xyz2[k * 3 + 2];
-        //         // float
-        //         //
-        //         d=max(sqrtf((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)+(z2-z1)*(z2-z1)),1e-20f);
-        //         double d = (x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1) +
-        //                    (z2 - z1) * (z2 - z1);
-        //         if (d < best1) {
-        //             best3 = best2;
-        //             besti3 = besti2;
-        //             best2 = best1;
-        //             besti2 = besti1;
-        //             best1 = d;
-        //             besti1 = k;
-        //         } else if (d < best2) {
-        //             best3 = best2;
-        //             besti3 = besti2;
-        //             best2 = d;
-        //             besti2 = k;
-        //         } else if (d < best3) {
-        //             best3 = d;
-        //             besti3 = k;
-        //         }
-        //     }
-        //     dist[j * 3] = best1;
-        //     idx[j * 3] = besti1;
-        //     dist[j * 3 + 1] = best2;
-        //     idx[j * 3 + 1] = besti2;
-        //     dist[j * 3 + 2] = best3;
-        //     idx[j * 3 + 2] = besti3;
-        // }
         xyz1 += n * 3;
         xyz2 += m * 3;
         dist += n * 3;

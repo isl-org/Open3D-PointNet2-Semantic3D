3d7d3c641f0163293ad12076d29ba171b5e6a71d
Yixing Lao <yixing.lao@gmail.com>
2018-12-20 01:17:08 -0800

    use tf op directly

--- kitti_predict.py
+++ kitti_predict.py
@@ -94,18 +94,12 @@ if __name__ == "__main__":
         # Interpolate to original point cloud
         start_time = time.time()
         dense_points = kitti_file_data.points
-        dense_labels = interpolate_dense_labels(
+        dense_labels = predictor.interpolate_labels(
             sparse_points=points_collector.reshape((-1, 3)),
             sparse_labels=pd_labels_collector.flatten(),
             dense_points=dense_points.reshape((-1, 3)),
         )
         timer["interpolate"] += time.time() - start_time
-        dense_labels_2 = predictor.interpolate_labels(
-            sparse_points=points_collector.reshape((-1, 3)),
-            sparse_labels=pd_labels_collector.flatten(),
-            dense_points=dense_points.reshape((-1, 3)),
-        )
-        np.testing.assert_allclose(dense_labels, dense_labels_2)
 
         start_time = time.time()
         # Save sparse point cloud with predicted labels

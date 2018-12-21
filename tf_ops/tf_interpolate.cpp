8213073f1a9474a8480cc8fd1b6fb5b7088b241c
Yixing Lao <yixing.lao@gmail.com>
2018-12-20 18:56:37 -0800

    knn supports k != 3

--- tf_ops/tf_interpolate.cpp
+++ tf_ops/tf_interpolate.cpp
@@ -72,36 +72,48 @@ void interpolate_label_cpu(int num_sparse_points, int num_dense_points,
         buffer_to_eigen_vector(sparse_points, num_sparse_points * 3);
     open3d::KDTreeFlann reference_kd_tree(reference_pcd);
 
-#ifdef _OPENMP
-#pragma omp parallel for schedule(static)
-#endif
+    // #ifdef _OPENMP
+    // #pragma omp parallel for schedule(static)
+    // #endif
     for (size_t j = 0; j < num_dense_points; ++j) {
         // Move vectors inside if using omp, outside if omp disabled
         std::vector<int> candidate_indices(knn);
         std::vector<double> candidate_dists(knn);
         Eigen::Vector3d target_point;
-        size_t target_point_idx = j * knn;
+        size_t target_point_idx = j * 3;
         target_point(0) = dense_points[target_point_idx];
         target_point(1) = dense_points[target_point_idx + 1];
         target_point(2) = dense_points[target_point_idx + 2];
-        reference_kd_tree.SearchKNN(target_point, knn, candidate_indices,
-                                    candidate_dists);
+        int knn_found = reference_kd_tree.SearchKNN(
+            target_point, knn, candidate_indices, candidate_dists);
 
         // Find most frequent label
         int max_count = 0;
         int most_frequent_label = -1;
         std::unordered_map<int, int> map_label_to_count;
         int label;
-        for (size_t k = 0; k < knn; ++k) {
+        // std::cout << "num_sparse_points: " << num_sparse_points << std::endl;
+        // std::cout << "index: " << j << "; ";
+        for (size_t k = 0; k < knn_found; ++k) {
+            // std::cout << "k: " << k
+            //           << "; candidate_indices[k]: " << candidate_indices[k];
             label = sparse_labels[candidate_indices[k]];
+            // std::cout << "; label:" << label << " ";
             map_label_to_count[label]++;
             if (map_label_to_count[label] > max_count) {
                 most_frequent_label = label;
                 max_count = map_label_to_count[label];
             }
         }
+        if (knn_found != knn) {
+            std::cout << "knn_found: " << knn_found << std::endl;
+            std::cout << "most_frequent_label: " << most_frequent_label
+                      << std::endl;
+        }
+
         dense_labels[j] = most_frequent_label;
     }
+    std::cout << "interpolate_label_cpu done" << std::endl;
 }
 
 class InterpolateLabelOp : public OpKernel {
@@ -149,6 +161,7 @@ class InterpolateLabelOp : public OpKernel {
                     errors::InvalidArgument("knn must be an int scalar"));
         auto knn_flat = knn_tensor.flat<int>();
         const int knn = knn_flat(0);
+        std::cout << "knn in C++: " << knn << std::endl;
 
         // Output: dense_labels
         Tensor *dense_labels_tensor = nullptr;

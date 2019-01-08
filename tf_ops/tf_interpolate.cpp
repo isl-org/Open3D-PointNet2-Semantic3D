2b6b8ce3786b82cf3848d9605e6a4066715d486f
Yixing Lao <yixing.lao@gmail.com>
2019-01-08 06:16:23 -0800

    revert to slow threenn_cpu

--- tf_ops/tf_interpolate.cpp
+++ tf_ops/tf_interpolate.cpp
@@ -211,8 +211,8 @@ REGISTER_OP("ThreeNN")
 // - indices (b, n, 3): (64, 8192, 3), for each input point in target_points,
 //   find 3 nearest neighbors in base_points and return the indexes in
 //   base_points, the "3" means "3" nearest neighbors
-void threenn_cpu(int b, int n, int m, const float *xyz1, const float *xyz2,
-                 float *dists, int *indices) {
+void threenn_cpu_fast(int b, int n, int m, const float *xyz1, const float *xyz2,
+                      float *dists, int *indices) {
     // OPENMP only sees benefits if b is large, e.g. b == 64
     // #ifdef _OPENMP
     // #pragma omp parallel for schedule(static)
@@ -240,6 +240,58 @@ void threenn_cpu(int b, int n, int m, const float *xyz1, const float *xyz2,
             dists[start_idx + 1] = three_dists[1];
             dists[start_idx + 2] = three_dists[2];
         }
+    }
+}
+
+void threenn_cpu(int b, int n, int m, const float *xyz1, const float *xyz2,
+                 float *dist, int *idx) {
+    for (int i = 0; i < b; ++i) {
+        for (int j = 0; j < n; ++j) {
+            float x1 = xyz1[j * 3 + 0];
+            float y1 = xyz1[j * 3 + 1];
+            float z1 = xyz1[j * 3 + 2];
+            double best1 = 1e40;
+            double best2 = 1e40;
+            double best3 = 1e40;
+            int besti1 = 0;
+            int besti2 = 0;
+            int besti3 = 0;
+            for (int k = 0; k < m; ++k) {
+                float x2 = xyz2[k * 3 + 0];
+                float y2 = xyz2[k * 3 + 1];
+                float z2 = xyz2[k * 3 + 2];
+                // float
+                // d=max(sqrtf((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)+(z2-z1)*(z2-z1)),1e-20f);
+                double d = (x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1) +
+                           (z2 - z1) * (z2 - z1);
+                if (d < best1) {
+                    best3 = best2;
+                    besti3 = besti2;
+                    best2 = best1;
+                    besti2 = besti1;
+                    best1 = d;
+                    besti1 = k;
+                } else if (d < best2) {
+                    best3 = best2;
+                    besti3 = besti2;
+                    best2 = d;
+                    besti2 = k;
+                } else if (d < best3) {
+                    best3 = d;
+                    besti3 = k;
+                }
+            }
+            dist[j * 3] = best1;
+            idx[j * 3] = besti1;
+            dist[j * 3 + 1] = best2;
+            idx[j * 3 + 1] = besti2;
+            dist[j * 3 + 2] = best3;
+            idx[j * 3 + 2] = besti3;
+        }
+        xyz1 += n * 3;
+        xyz2 += m * 3;
+        dist += n * 3;
+        idx += n * 3;
     }
 }
 

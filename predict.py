1ae80db8005665ecd1d56e534341af26d5ceeb98
Yixing Lao <yixing.lao@gmail.com>
2019-01-16 16:04:08 -0800

    predicts all files

--- predict.py
+++ predict.py
@@ -145,7 +145,7 @@ if __name__ == "__main__":
     # Process each file
     cm = ConfusionMatrix(9)
 
-    for semantic_file_data in dataset.list_file_data[:1]:
+    for semantic_file_data in dataset.list_file_data:
         print("Processing {}".format(semantic_file_data))
 
         # Predict for num_samples times
